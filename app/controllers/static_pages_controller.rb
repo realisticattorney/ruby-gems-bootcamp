40046e1e1c6d8165503bd5ba83e9e59f2d1239a5
realisticattorney <g.axl.aquila@gmail.com>
2021-02-12 20:37:12 +0000

    render courses at homepage using loops and selecting by title and created at

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,13 @@
 class StaticPagesController < ApplicationController
   def landing_page
+    @courses = Course.all.limit(3)
     
+    @latest_courses = Course.all.limit(3).order(created_at: :desc)
   end
 
   def privacy_policy
   
   end
+  
+  
 end
