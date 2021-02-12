56ee1fbd7d1f4cd317dd60c009aa33566fcfa5a8
realisticattorney <g.axl.aquila@gmail.com>
2021-02-12 05:47:39 +0000

    add author to seeded courses, add current user to controller action create

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,9 @@
+User.create!(email:"admin@example.com", password:"german1", password_confirmation:"german1")
+
 30.times do
   Course.create!([{
     title: Faker::Educator.course_name,
-    description: Faker::TvShows::GameOfThrones.quote
+    description: Faker::TvShows::GameOfThrones.quote,
+    user_id: User.first.id
   }])
 end
